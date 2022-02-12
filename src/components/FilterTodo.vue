092ded02685f5ad172cd1e5c37ca3501574bee69
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2022-02-12 09:19:49 +0600

    Update FilterTodo.vue

--- src/components/FilterTodo.vue
+++ src/components/FilterTodo.vue
@@ -2,18 +2,45 @@
   <div class="filter">
     <div class="filter__left">
       <button
-        class="filter__button filter__button--active"
-        @click="$emit('allTodo')"
+        class="filter__button"
+        :class="{
+          'filter__button--active': all,
+        }"
+        @click="
+          $emit('allTodo');
+          all = true;
+          done = false;
+          pending = false;
+        "
       >
         All ({{ allTodosLength }})
       </button>
       <button
         class="filter__button"
-        @click="filter__mode = $emit('pendingTodo')"
+        :class="{
+          'filter__button--active': pending,
+        }"
+        @click="
+          filter__mode = $emit('pendingTodo');
+          all = false;
+          done = false;
+          pending = true;
+        "
       >
         Pending ({{ allPendingTodos }})
       </button>
-      <button class="filter__button" @click="filter__mode = $emit('doneTodo')">
+      <button
+        class="filter__button"
+        :class="{
+          'filter__button--active': done,
+        }"
+        @click="
+          filter__mode = $emit('doneTodo');
+          all = false;
+          done = true;
+          pending = false;
+        "
+      >
         Done ({{ allDoneTodos }})
       </button>
     </div>
@@ -41,6 +68,13 @@ export default {
     allDoneTodos: {
       type: Number,
     },
+  },
+  data() {
+    return {
+      all: true,
+      pending: false,
+      done: false,
+    };
   },
 };
 </script>
